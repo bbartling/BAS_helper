5bfc10ba8193c8eb5b815c5eccbbb9c730971eeb
bbartling <ben.bartling@gmail.com>
2021-04-18 09:02:39 -0500

    Add files via upload

--- fake_data/faker.py
+++ fake_data/faker.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-'''
-thanks Andrew Rodgers at Ace IoT
-for some coding tips on writing a better script
-'''
+
 
 import BAC0
 import random
@@ -29,6 +25,9 @@ bacnet = BAC0.lite()
 
 
 '''
+thanks Andrew Rodgers at Ace IoT
+for some coding tips on writing a better script
+
 Create writeable duct pressure setpoint below. This is where trim respond
 logic from othersource to write to this point. In real worldthis pressure
 setpoint would exist on an AHU controller for a VAV system per this sequence:
@@ -56,62 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-'''
-thanks Andrew Rodgers at Ace IoT
-for some coding tips on writing a better script
-'''
-
-import BAC0
-import random
-import time
-from BAC0.core.devices.create_objects import create_AO,create_AV,create_AI
-from BAC0.tasks.RecurringTask import RecurringTask 
-from bacpypes.primitivedata import Real
-
-# create simulated 10 VAV box damper outputs
-bacnet_objects = [{"name": f"DPR{i}-O"} for i in range(10)]
-for i, point in enumerate(bacnet_objects):
-  point['object'] = create_AO(oid=i, name=point['name'], pv=Real(11*(1+i)))
-  point['random_args'] = (5, 95, 1)
-  point['label'] = f"VAV damper {i+1} position"
-
-def changeValues():
-  for point in bacnet_objects:
-     value = random.randrange(*point['random_args'])
-     point['object'].presentValue = Real(value)
-     
-# create app
-bacnet = BAC0.lite()
-
-
-
-'''
-Create writeable duct pressure setpoint below. This is where trim respond
-logic from othersource to write to this point. In real worldthis pressure
-setpoint would exist on an AHU controller for a VAV system per this sequence:
-https://www.taylor-engineering.com/wp-content/uploads/2020/04/ASHRAE_Journal_-_Trim_and_Respond.pdf
-
-'''
-for point in bacnet_objects:
-  bacnet.this_application.add_object(point['object'])
-bacnet.this_application.add_object(create_AV(oid=10, name='DAP-SP',  pv=1, pv_writable=True))
-
-
-
-def main():
-    task1 = RecurringTask(changeValues,delay=60)
-    task1.start()
-
-    while True:
-      for point in bacnet_objects:
-        bac0_object = bacnet.this_application.get_object_name(point['name'])
-        bacnet._log.info(f"{point['label']} is {bac0_object.presentValue}")
-        duct_pres_sp = bacnet.this_application.get_object_name('DAP-SP')
-        bacnet._log.info('Duct Pressure Setpoint is {}'.format(duct_pres_sp.presentValue))
-        time.sleep(120)
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 20dc0410b79062fc98ef42881970ed3719c796c9
--- fake_data/faker_new.py
+++ fake_data/faker_new.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-'''
-
-Create writeable duct pressure setpoint below. This is where trim respond
-logic from othersource to write to this point. In real worldthis pressure
-setpoint would exist on an AHU controller for a VAV system per this sequence:
-https://www.taylor-engineering.com/wp-content/uploads/2020/04/ASHRAE_Journal_-_Trim_and_Respond.pdf
-
-'''
-
 import BAC0,time,random
 
 from BAC0.core.devices.local.models import (
@@ -31,7 +21,6 @@ def changeValues():
      
      
 # create simulated 10 VAV box damper outputs
-
 for i in range(10):
 
     _new_objects = analog_output(
@@ -52,8 +41,9 @@ _new_objects = analog_value(
 
      
 # create app
-bacnet = BAC0.lite(ip='10.0.2.20/24',deviceId='2021')
-
+#bacnet = BAC0.lite(ip='10.0.2.20/24',deviceId='2021')
+bacnet = BAC0.lite()
+  
 _new_objects.add_objects_to_application(bacnet)
 
 bacnet._log.info("APP Created Success!")
@@ -75,81 +65,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-'''
-
-Create writeable duct pressure setpoint below. This is where trim respond
-logic from othersource to write to this point. In real worldthis pressure
-setpoint would exist on an AHU controller for a VAV system per this sequence:
-https://www.taylor-engineering.com/wp-content/uploads/2020/04/ASHRAE_Journal_-_Trim_and_Respond.pdf
-
-'''
-
-import BAC0,time,random
-
-from BAC0.core.devices.local.models import (
-    analog_output,
-    analog_value
-    )
-
-from BAC0.tasks.RecurringTask import RecurringTask 
-from bacpypes.primitivedata import Real
-
-
-
-
-
-def changeValues():
-  for i in range(10):
-     vav_object = bacnet.this_application.get_object_name(f"DPR{i}-O")
-     value = random.randrange(5, 95, 1)
-     vav_object.presentValue = Real(value)
-     
-     
-     
-# create simulated 10 VAV box damper outputs
-
-for i in range(10):
-
-    _new_objects = analog_output(
-    name=f"DPR{i}-O",
-    properties={"units": "percent"},
-    description=f"VAV Box {i} Damper Output",
-    presentValue=50,is_commandable=False
-    )
-  
-
-# create AHU duct pressure setpoint point
-_new_objects = analog_value(
-        name="DAP-SP",
-        properties={"units": "inchesOfWater"},
-        description="AHU Duct Pressure Setpoint",
-        presentValue=1,is_commandable=True
-    )
-
-     
-# create app
-bacnet = BAC0.lite(ip='10.0.2.20/24',deviceId='2021')
-
-_new_objects.add_objects_to_application(bacnet)
-
-bacnet._log.info("APP Created Success!")
-
-
-
-def main():
-    task1 = RecurringTask(changeValues,delay=60)
-    task1.start()
-
-    while True:
-      for i in range(10):
-        bac0_object = bacnet.this_application.get_object_name(f"DPR{i}-O")
-        bacnet._log.info(f"DPR{i}-O is {bac0_object.presentValue}")
-        duct_pres_sp = bacnet.this_application.get_object_name('DAP-SP')
-        bacnet._log.info('Duct Pressure Setpoint is {}'.format(duct_pres_sp.presentValue))
-        time.sleep(10)
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 20dc0410b79062fc98ef42881970ed3719c796c9
